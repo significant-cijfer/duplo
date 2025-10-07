7f4b546be6d486698e876e4363094237166505cf
significant-cijfer <fake.potsjem@gmail.com>
2025-10-07 23:43:00 +0200

    [wip] Very first layer of examination implemented

--- src/context.zig
+++ src/context.zig
@@ -3,7 +3,7 @@ const panic = std.debug.panic;
 
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
-const StringHashMap = std.StringHashMap;
+const StringHashMap = std.StringHashMapUnmanaged;
 
 const Parser = @import("parser.zig");
 const Ast = Parser.Ast;
@@ -32,12 +32,13 @@ pub const Typx = struct {
 };
 
 pub const Context = struct {
+    allocator: Allocator,
     table: StringHashMap(Symbol),
     types: ArrayList(Typx),
 
     const Symbol = struct {
         scope: Scope,
-        typx: Typx,
+        typx: u32,
     };
 
     const Scope = enum {
@@ -47,20 +48,31 @@ pub const Context = struct {
     };
 
     pub fn deinit(self: *Context) void {
-        self.table.deinit();
+        self.table.deinit(self.allocator);
     }
 
     fn pushTypx(self: *Context, typx: Typx) !u32 {
+        const idx = self.types.items.len;
+        try self.types.append(self.allocator, typx);
+        return @intCast(idx);
     }
 
     fn examine(self: *Context, tree: Ast, idx: u32) !u32 {
         const node = tree.nodes.items[idx];
 
-        _ = self;
-
         switch (node.kind) {
             .fdecl => {
-                return 0;
+                const proto = try self.examine(tree, node.extra.fdecl.proto);
+                const body = try self.examine(tree, node.extra.fdecl.body);
+                _ = body;
+
+                return proto;
+            },
+            .fproto => {
+                return try self.pushTypx(.{
+                    .kind = .function,
+                    .extra = .{ .function = .{} },
+                });
             },
             else => panic("Unhandled examination: {}", .{node.kind}),
         }
@@ -69,7 +81,8 @@ pub const Context = struct {
 
 pub fn scan(gpa: Allocator, tree: Ast) !Context {
     var ctx = Context{
-        .table = .init(gpa),
+        .allocator = gpa,
+        .table = .empty,
         .types = .empty,
     };
 
